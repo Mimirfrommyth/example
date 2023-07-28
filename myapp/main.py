aa703c8caf00e295424d54e1f646aac29a8f4754
Akhil Cherukuri <chakhilsatyasai225@gmail.com>
2023-07-28 12:30:12 -0500

    third commit

--- myapp/main.py
+++ myapp/main.py
@@ -5,13 +5,10 @@ from flask_login import UserMixin
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://example_pqya_user:qLoewjGUu7slEM4eVXoFH8k436mKM1Ws@dpg-cj1v4295rnuhn3jaamm0-a.oregon-postgres.render.com/example_pqya'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://example_pqya_user:qLoewjGUu7slEM4eVXoFH8k436mKM1Ws@dpg-cj1v4295rnuhn3jaamm0-a.oregon-postgres.render.com/example_pqya'
     # postgres://example_pqya_user:qLoewjGUu7slEM4eVXoFH8k436mKM1Ws@dpg-cj1v4295rnuhn3jaamm0-a.oregon-postgres.render.com/example_pqya
-    db = SQLAlchemy(app)
+    #db = SQLAlchemy(app)
 
-    class User(UserMixin, db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        email = db.Column(db.String(100), unique=True)
         
     #Line below only required once, when creating DB.
     # with app.app_context():
